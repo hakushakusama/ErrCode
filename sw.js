3f053cc0680b50992e79be67efac0bdd68b7a513
max__robots <ama.hakushaku.2012@gmail.com>
2025-12-24 05:56:28 +0100

    check-pwa.js

--- sw.js
+++ sw.js
@@ -14,7 +14,12 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open("errorcodes-cache").then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
